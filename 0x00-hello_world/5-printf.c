02918bac711815f1d2753d966ed250a3cb6d9fdf
jobaba46 <jobaba46@gmail.com>
2022-07-01 00:23:21 +0000

    F

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pont
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
