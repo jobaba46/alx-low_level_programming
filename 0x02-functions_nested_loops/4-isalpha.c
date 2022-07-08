6e411afac30cdc1b54eca6de47ac89575d68235c
jobaba46 <jobaba46@gmail.com>
2022-07-08 18:48:59 +0000

    3lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "manin.h"
+
+/**
+ * _isalpha - check for alphaetical letter
+ * @c: a charter to be check on
+ *  Return: returns 0 or 1 depending on condition
+ */
+int _isalpha(int c)
+{
+	return((c >= 'a' && c <= 'Z') || (c >= 'A' && c <= 'Z'))
+}
