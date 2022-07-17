e82014e72423eb286d5f73da08e7ef4dca73bd0f
jobaba46 <jobaba46@gmail.com>
2022-07-17 15:36:30 +0000

    workin

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+
+void reset_to_98(int *);
 
 /**
- * reset_to_98-> a function to update the reference of a pointer
- * @n: pointer to n to be updated
- */
-void reset_to_98(int *n);
+*main - check the code for Holberton School students
+*
+* Return: Always 0.
+*/
+int main(void)
 {
-   *n 98;
+	int n;
+
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }
